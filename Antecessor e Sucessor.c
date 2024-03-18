75b1307422390f68260c1e8131913180dad7508a
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2024-03-18 10:26:16 -0300

    Add files via upload

---
+++ Antecessor e Sucessor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main(){
+	// Acentos nas palavras
+	setlocale(LC_ALL, "portuguese");
+	
+	// Variaveis
+	int numero;
+	
+	// Solicitação de dados
+	printf("Digite o numero desejado: ");
+	scanf("%i", &numero);
+	
+	system("cls || clear");
+	
+	// Calculos
+	int numeroAntecessor = numero - 1;
+	int numeroSucessor = numero + 1;
+	
+	
+	// Resultados
+	printf("O numero antecessor ao seu numero escolhido é: %i \n", numeroAntecessor);
+	printf("O numero sucessor ao seu numero escolhido é: %i \n", numeroSucessor);
+	
+	return 0;
+}
