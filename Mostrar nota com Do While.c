864d00726bf6d915671a1c06ea4e9c7a1d07bb72
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2024-03-26 09:10:46 -0300

    Add files via upload

---
+++ Mostrar nota com Do While.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <locale.h>
+# include <string.h>
+
+int main() {
+	// Acentos nas palavras
+	setlocale(LC_ALL, "portuguese");
+
+	// Variaveis
+	int i = 0;
+	float nota;
+
+	// Solicitação
+	do {
+		printf("Digite sua nota: ");
+		scanf("%f", &nota);
+		
+		i++;
+	} while (nota < 0 || nota > 10);
+	
+	// Resultados
+	printf("\nSua nota foi: %1.f", nota);
+	return 0;
+}
