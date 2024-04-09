db1f22a55025900eebc3c1e285e0f8bc98cd1d62
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2024-04-09 10:49:12 -0300

    Add files via upload

---
+++ Aprovado com Vetor.c
@@ -0,0 +1,55 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <locale.h>
+# include <string.h>
+# include <ctype.h>
+# include <time.h>
+# include <limits.h> 
+
+# define TAM 4
+
+int main() {
+	setlocale(LC_ALL, "portuguese");
+	
+	// Variaveis
+	float notas[TAM], soma = 0, media;
+	int contador;
+	char reiniciar;
+
+	do{
+		for (contador = 0; contador < TAM; contador++) {
+			printf("Informe sua %i° nota: ", contador + 1);
+			scanf("%f", &notas[contador]);
+			soma += notas[contador];
+		}
+		system("cls || clear");
+		
+		// Calculos 
+		media = soma /TAM;
+	
+		// Resultados
+		printf("Suas notas foram: \n");
+		for (contador = 0; contador < TAM; contador++) {
+			printf("%iº nota: %.2f \n", contador + 1, notas[contador]);	
+		}
+		printf("Enquanto sua media foi: %.2f \n", media);
+		if (media >= 7) {
+			printf("Você foi aprovado, parabens!");
+		} else if (media >= 5 ) {
+			printf("Infelizmente você foio para a recuperação!");
+		} else {
+			printf("Infelizmente você foi reprovado!");
+		}
+	
+	
+		// Reiniciar ou Fechar código
+		printf("\n\nDeseja continuar com esse código? [S/N] \n");	
+		scanf("%s", &reiniciar);
+		reiniciar = toupper(reiniciar);
+		soma = 0, media = 0;
+		system("cls || clear");
+	
+	} while (reiniciar == 'S');
+	
+	return 0;
+}
